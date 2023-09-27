c61d245296e30ffe6776043a07035d53583508d3
Sergio Redondo <rodo1985@gmail.com>
2023-09-27 17:54:37 +0200

    Added MLFlow

--- validation.py
+++ validation.py
@@ -1,17 +1,32 @@
 import cv2
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 from ultralytics import YOLO
+import mlflow
+
+
+# set to True to draw anotations
+draw_anotations = False
+
+#mlflow server --backend-store-uri sqlite:///mlflow.db
+# mlflow.set_tracking_uri("sqlite:////home/jioy/PROJECTS/Tensorboard/mlflow.db")
+mlflow.set_experiment("New annotation")
+
 
 # load the model
 model = YOLO('datasets/runs/pose/train18/weights/best.pt')
 
 # path to the folder containing the images
-image_folder = 'datasets/dataset/val/images'
+image_folder = 'datasets/dataset/train/images'
 
 # read all images and annotations in image_folder
 images = os.listdir(image_folder)
 annotations = [image.replace('tif', 'txt') for image in images]
+
+# arrays of results
+x_diff_array  = np.zeros([13, len(images)])
+y_diff_array = np.zeros([13, len(images)])
 
 # loop over all images
 for ind in range(len(images)):
@@ -58,8 +73,6 @@ for ind in range(len(images)):
     keypoint_size = 9  # Diameter of the drawn keypoints
     keypoint_color = (255, 0, 0)  # Red keypoints
 
-    print(len(keypoints)/3)
-
     for i in range(0, len(keypoints), 3):
         x, y, visibility = map(float, keypoints[i:i + 3])
         if visibility == 2:  # Visible keypoint
@@ -68,16 +81,40 @@ for ind in range(len(images)):
             cv2.circle(image, (x, y), keypoint_size, keypoint_color, -1)
 
             # calculate distance between predicted and ground truth keypoints
-            x_diff = abs(x - int(yolo_keypoints.xy[0][int(i/3)][0]))
-            y_diff = abs(y - int(yolo_keypoints.xy[0][int(i/3)][1]))
-            print(x_diff, y_diff)
+            x_diff = abs(x - yolo_keypoints.xy[0][int(i/3)][0])
+            y_diff = abs(y - yolo_keypoints.xy[0][int(i/3)][1])
+
+            x_diff_array[int(i/3), ind] = x_diff
+            y_diff_array[int(i/3), ind] = y_diff
 
 
     # Convert BGR image to RGB
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Plot the image using Matplotlib
-    plt.imshow(image_rgb)
-    plt.axis('off')  # Turn off axis numbers and ticks
-    plt.get_current_fig_manager().full_screen_toggle()
-    plt.show()
+    if draw_anotations:
+        # Plot the image using Matplotlib
+        plt.imshow(image_rgb)
+        plt.axis('off')  # Turn off axis numbers and ticks
+        plt.get_current_fig_manager().full_screen_toggle()
+        plt.show()
+
+keys = [
+    't1_top', 't1_bottom', 't2_top', 't2_bottom', 
+    'weld_position', 'weld_center', 'weld_left', 'weld_right', 'weld_bottom', 
+    'concavity', 't1_top_new', 't2_top_left', 't2_top_right'
+]
+
+results = {}
+
+for key in keys:
+    for prefix, array_index in zip(['mean', 'std', 'max'], range(3)):
+        x_key = f'{prefix}_error_{key}_x'
+        y_key = f'{prefix}_error_{key}_y'
+        e_key = f'{prefix}_error_{key}_e'
+        
+        results[x_key] = np.mean(x_diff_array[array_index, :])
+        results[y_key] = np.mean(y_diff_array[array_index, :])
+        results[e_key] = np.sqrt(np.mean(x_diff_array[array_index, :]**2 + y_diff_array[array_index, :]**2))
+
+
+mlflow.log_metrics(results)
